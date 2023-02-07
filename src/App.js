012753dcb76bbd7a1b180a677e58aa5eb1eaaebe
Jitender Kumar <114587346+ohyajk@users.noreply.github.com>
2023-02-07 17:52:38 +0530

    Add Functionality and Style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Details from './Components/Details';
+import Home from './Components/Home';
+import Navbar from './Components/Navbar';
+import { fetchData } from './Redux/Coins/CoinSlice';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchData());
+  }, [dispatch]);
+
   return (
-    <div className="underline">
-      123
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/coin" element={<Details />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
