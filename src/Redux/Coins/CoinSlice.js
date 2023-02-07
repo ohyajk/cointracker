c117f3f8353c13860507e1e94178c4bc54293191
Jitender Kumar <114587346+ohyajk@users.noreply.github.com>
2023-02-07 17:58:48 +0530

    Fix no param error

--- src/Redux/Coins/CoinSlice.js
+++ src/Redux/Coins/CoinSlice.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 export const fetchData = createAsyncThunk('data/fetchData', async () => {
